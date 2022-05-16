261e9e4274f2b3fdcdfa9c93b47dffa046f9f858
everest <liamwaldron7009@protonmail.com>
2022-05-16 22:55:39 +0000

    Update INSTALL.sh

--- install/INSTALL.sh
+++ install/INSTALL.sh
@@ -4,5 +4,4 @@ chmod +x ../scripts/now.sh
 cp ../scripts/now.sh ../scripts/now
 mv ../scripts/now /usr/bin
 mv ../scripts/now.conf /etc
-mkdir /home/$USER/.config/now
-mv ../scripts/events.sh /home/$USER/.config/now
+mv ../scripts/events.sh .config/now
